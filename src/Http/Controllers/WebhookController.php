fb68b693b7c96a5371df51b31d07f1c7691585a9
Tim Oladoyinbo <dev@digitalkraaft.com>
2020-07-17 00:40:51 +0100

    webhooks update

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -1,55 +1,11 @@
 <?php
-
-
 namespace Digikraaft\PaystackSubscription\Http\Controllers;
 
-use Digikraaft\PaystackSubscription\Events\WebhookHandled;
-use Digikraaft\PaystackSubscription\Events\WebhookReceived;
-use Digikraaft\PaystackSubscription\Http\Middleware\VerifyWebhookSignature;
 use Digikraaft\PaystackSubscription\PaystackSubscription;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
-use Illuminate\Support\Str;
-use Symfony\Component\HttpFoundation\Response;
+use Digikraaft\PaystackWebhooks\Http\Controllers\WebhooksController as WebhooksController;
 
-class WebhookController extends Controller
+class WebhookController extends WebhooksController
 {
-    /**
-     * Create a new WebhookController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        if (config('paystacksubscription.secret', env('PAYSTACK_SECRET'))) {
-            $this->middleware(VerifyWebhookSignature::class);
-        }
-    }
-
-    /**
-     * Handle Paystack webhook call.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function handleWebhook(Request $request)
-    {
-        $payload = json_decode($request->getContent(), true);
-        $method = 'handle'.Str::studly(str_replace('.', '_', $payload['event']));
-
-        WebhookReceived::dispatch($payload);
-
-        if (method_exists($this, $method)) {
-            $response = $this->{$method}($payload);
-
-            WebhookHandled::dispatch($payload);
-
-            return $response;
-        }
-
-        return $this->missingMethod();
-    }
-
     /**
      * Handle disabled customer subscription.
      *
@@ -137,27 +93,5 @@ class WebhookController extends Controller
     protected function getUserByPaystackId(string $paystackId)
     {
         return PaystackSubscription::findBillable($paystackId);
-    }
-
-    /**
-     * Handle successful calls on the controller.
-     *
-     * @param  array  $parameters
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    protected function successMethod($parameters = [])
-    {
-        return new Response('Webhook Handled', 200);
-    }
-
-    /**
-     * Handle calls to missing methods on the controller.
-     *
-     * @param  array  $parameters
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    protected function missingMethod($parameters = [])
-    {
-        return new Response;
     }
 }
